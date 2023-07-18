18ffc522b0d7b8d5667fcaa94feed977c33c82ad
zednegr <negrzed@gmail.com>
2023-07-18 13:29:39 +0500

    modal

---
+++ src/components/header/header.jsx
@@ -0,0 +1,27 @@
+import './header.scss'
+
+function Header() {
+    
+    return (
+      
+        <div className="section">
+            <div className="wrapper">
+                <div className="modal">
+                    <div className="data">
+
+                    </div>
+                    <div className="quantity">
+                        <div className="modal-text">Miqdori</div>
+                        <div className="sum">1,000,000 UZS</div>
+                    </div>
+                    <div className="buttons">
+                        <button>SOTIB OLISH</button>
+                        <button>SAVATGA OTISH</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
